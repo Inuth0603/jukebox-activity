2fe6368cc94b084dac58df4a83ad401ac7fbb7a3
Divyam Agarwal <iitdivyamagarwal@gmail.com>
2026-07-25 22:53:40 +0530

    feat: Port emptypanel.py to GTK4
    Migrate the empty-state placeholder UI from GTK3 to GTK4.
    
    Replace Gtk.EventBox + modify_bg() with a plain Gtk.Box using the
    background-white CSS class from sugar-artwork - modify_bg() is gone
    in GTK4 and the plain Box was showing through as transparent without
    an explicit background class.
    
    Update sugar3 imports to sugar4, stock icons to icon-name loading,
    and pack_start() padding/expand flags to set_margin/halign/vexpand -
    mechanical, no behavior change.

--- emptypanel.py
+++ emptypanel.py
@@ -1,43 +1,53 @@
 from gi.repository import Gtk
 
-from sugar3.graphics import style
-from sugar3.graphics.icon import Icon
+from sugar4.graphics import style
+from sugar4.graphics.icon import Icon
 
 
 def show(activity, icon_name, message, btn_label, btn_callback):
-    empty_widgets = Gtk.EventBox()
-    empty_widgets.modify_bg(Gtk.StateType.NORMAL,
-                            style.COLOR_WHITE.get_gdk_color())
+    empty_widgets = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+    empty_widgets.add_css_class("background-white")
+    empty_widgets.set_vexpand(True)
+    empty_widgets.set_hexpand(True)
 
-    vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-    mvbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-    vbox.pack_start(mvbox, True, False, 0)
+    center_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+    center_box.set_valign(Gtk.Align.CENTER)
+    center_box.set_halign(Gtk.Align.CENTER)
+    center_box.set_vexpand(True)
+    center_box.set_hexpand(True)
+    empty_widgets.append(center_box)
 
     image_icon = Icon(pixel_size=style.LARGE_ICON_SIZE,
                       icon_name=icon_name,
                       stroke_color=style.COLOR_BUTTON_GREY.get_svg(),
                       fill_color=style.COLOR_TRANSPARENT.get_svg())
-    mvbox.pack_start(image_icon, False, False, style.DEFAULT_PADDING)
+    image_icon.set_margin_bottom(style.DEFAULT_PADDING)
+    center_box.append(image_icon)
 
-    label = Gtk.Label('<span foreground="%s"><b>%s</b></span>' %
+    label = Gtk.Label(label='<span foreground="%s"><b>%s</b></span>' %
                       (style.COLOR_BUTTON_GREY.get_html(),
                        message))
     label.set_use_markup(True)
-    mvbox.pack_start(label, False, False, style.DEFAULT_PADDING)
+    label.set_margin_bottom(style.DEFAULT_PADDING)
+    center_box.append(label)
 
     hbox = Gtk.Box()
+    hbox.set_halign(Gtk.Align.CENTER)
     open_image_btn = Gtk.Button()
     open_image_btn.connect('clicked', btn_callback)
-    add_image = Gtk.Image.new_from_stock(Gtk.STOCK_ADD,
-                                         Gtk.IconSize.BUTTON)
-    buttonbox = Gtk.Box()
-    buttonbox.pack_start(add_image, False, True, 0)
-    buttonbox.pack_end(Gtk.Label(btn_label), True, True, 5)
-    open_image_btn.add(buttonbox)
-    hbox.pack_start(open_image_btn, True, False, 0)
-    mvbox.pack_start(hbox, False, False, style.DEFAULT_PADDING)
 
-    empty_widgets.add(vbox)
-    empty_widgets.show_all()
+    add_image = Gtk.Image.new_from_icon_name("list-add")
+    buttonbox = Gtk.Box(spacing=8)
+    buttonbox.append(add_image)
+    lbl = Gtk.Label(label=btn_label)
+    lbl.set_margin_start(5)
+    lbl.set_margin_end(5)
+    buttonbox.append(lbl)
+
+    open_image_btn.set_child(buttonbox)
+    hbox.append(open_image_btn)
+    hbox.set_margin_bottom(style.DEFAULT_PADDING)
+    center_box.append(hbox)
+
     activity.view_area.append_page(empty_widgets, None)
     activity.view_area.set_current_page(2)
